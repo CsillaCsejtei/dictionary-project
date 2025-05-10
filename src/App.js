e8ab4e77aa96415a06de608e4929c0f423c5e6c6
CsillaCsejtei <csillacsejtei@gmail.com>
2025-05-10 08:42:29 +0100

    Added search engine

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
- import './App.css';
+import "./App.css";
+import Dictionary from "./Dictionary.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Welcome to my App
-      </header>
+      <div className="container">
+        <header className="App-header">Welcome to my App</header>
+        <main><Dictionary/></main>
+        <footer className="App-footer"><small>Coded by Csilla Csejtei</small></footer>
+      </div>
     </div>
   );
 }
